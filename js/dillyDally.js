c9a73dd74aa950c7d67627883290fb42f4f3e7c5
LouBNielsen <louise.b.nielsen@hotmail.com>
2020-01-24 10:20:39 +0100

    close #490 implement validation for time registration

--- js/dillyDally.js
+++ js/dillyDally.js
@@ -1,12 +1,70 @@
 $(function () {
-    $('input:radio').change(function () {
+    $("#notRelatedToGithubIssue").hide();
+    $("#relatedToGithubIssue").hide();
+    $("input[type='radio'][name='issue']").change(function () {
         if ($(this).val() == "yes") {
             $("#relatedToGithubIssue").show();
             $("#notRelatedToGithubIssue").hide();
         }
-        else {
+        else if ($(this).val() == "no") {
             $("#notRelatedToGithubIssue").show();
+            $("#relatedToGithubIssue").hide();
+        } else {
+            $("#notRelatedToGithubIssue").hide();
             $("#relatedToGithubIssue").hide();
         }
     });
 });
+
+function validatePart1() {
+    let allAreFilled = true;
+    document.getElementById("timereg_form_part1").querySelectorAll("[required]").forEach(function (element) {
+        if (element.value == "") {
+            element.setAttribute("style", "border: 1px solid red;");
+        } else {
+            element.setAttribute("style", "border: 1px solid #ccc;");
+        }
+
+        if (!element.value) allAreFilled = false;
+    })
+    return allAreFilled;
+}
+function validatePart2a() {
+    validatePart1();
+    let allAreFilled = true;
+    document.getElementById("timereg_form_part2a").querySelectorAll("[required]").forEach(function (element) {
+        if (element.value == "") {
+            element.setAttribute("style", "border: 1px solid red;");
+        } else {
+            element.setAttribute("style", "border: 1px solid #ccc;");
+        }
+
+        if (!element.value) allAreFilled = false;
+
+    })
+    if (!allAreFilled) return;
+}
+function validatePart2b() {
+    validatePart1();
+    let allAreFilled = true;
+    document.getElementById("timereg_form_part2b").querySelectorAll("[required]").forEach(function (element) {
+        if (element.value == "") {
+            element.setAttribute("style", "border: 1px solid red;");
+        } else {
+            element.setAttribute("style", "border: 1px solid #ccc;");
+        }
+
+        if (!element.value) allAreFilled = false;
+
+    })
+    if (!allAreFilled) return;
+}
+
+/// Required radio button
+// if (element.type === "radio") {
+//     let radioValueCheck = false;
+//     document.getElementById("timereg_form_part2b").querySelectorAll(`[name=${element.name}]`).forEach(function (radio) {
+//         if (radio.checked) radioValueCheck = true;
+//     })
+//     allAreFilled = radioValueCheck;
+// }
