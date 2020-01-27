2abdc85671e761e77c72f98cdf193368a2450667
LouBNielsen <louise.b.nielsen@hotmail.com>
2020-01-27 14:15:17 +0100

    close #497 post dilly dally form data with dillydally api function

--- js/dillyDally.js
+++ js/dillyDally.js
@@ -1,7 +1,7 @@
 $(function () {
     $("#notRelatedToGithubIssue").hide();
     $("#relatedToGithubIssue").hide();
-    $("input[type='radio'][name='issue']").change(function () {
+    $("input[type='radio'][name='githubIssue']").change(function () {
         if ($(this).val() == "yes") {
             $("#relatedToGithubIssue").show();
             $("#notRelatedToGithubIssue").hide();
@@ -9,16 +9,30 @@ $(function () {
         else if ($(this).val() == "no") {
             $("#notRelatedToGithubIssue").show();
             $("#relatedToGithubIssue").hide();
-        } else {
-            $("#notRelatedToGithubIssue").hide();
-            $("#relatedToGithubIssue").hide();
         }
     });
 });
 
-function validatePart1() {
+function validatePart2a() {
+    let form1 = validate("timereg_form_part1");
+    let form2 = validate("timereg_form_part2a")
+
+    if (form1 && form2) {
+        submitform($("#timereg_form").serializeArray())
+    }
+}
+function validatePart2b() {
+    let form1 = validate("timereg_form_part1");
+    let form2 = validate("timereg_form_part2b")
+
+    if (form1 && form2) {
+        submitform($("#timereg_form").serializeArray())
+    }
+}
+
+function validate(element) {
     let allAreFilled = true;
-    document.getElementById("timereg_form_part1").querySelectorAll("[required]").forEach(function (element) {
+    document.getElementById(element).querySelectorAll("[required]").forEach(function (element) {
         if (element.value == "") {
             element.setAttribute("style", "border: 1px solid red;");
         } else {
@@ -26,39 +40,32 @@ function validatePart1() {
         }
 
         if (!element.value) allAreFilled = false;
+
     })
     return allAreFilled;
 }
-function validatePart2a() {
-    validatePart1();
-    let allAreFilled = true;
-    document.getElementById("timereg_form_part2a").querySelectorAll("[required]").forEach(function (element) {
-        if (element.value == "") {
-            element.setAttribute("style", "border: 1px solid red;");
-        } else {
-            element.setAttribute("style", "border: 1px solid #ccc;");
+
+function submitform(data) {
+    var fields = Object.assign({}, ...data.map(item => ({ [item.name]: item.value, })));
+    var workhours = '{"type": "workhours", "fields" : ' + JSON.stringify(fields) + ' }';
+
+    axios({
+        method: 'post',
+        url: 'https://us-central1-dillydally-stage.cloudfunctions.net/api',
+        data: workhours,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         }
-
-        if (!element.value) allAreFilled = false;
-
     })
-    if (!allAreFilled) return;
+        .then((res) => {
+            console.log(res.status + " " + res.statusText); // Todo synglig fejlhåndtering i frontend
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
 }
-function validatePart2b() {
-    validatePart1();
-    let allAreFilled = true;
-    document.getElementById("timereg_form_part2b").querySelectorAll("[required]").forEach(function (element) {
-        if (element.value == "") {
-            element.setAttribute("style", "border: 1px solid red;");
-        } else {
-            element.setAttribute("style", "border: 1px solid #ccc;");
-        }
 
-        if (!element.value) allAreFilled = false;
-
-    })
-    if (!allAreFilled) return;
-}
 
 /// Required radio button
 // if (element.type === "radio") {
