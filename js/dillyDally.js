5d4a69b766ae7d88fffff2edbe93bce232c8a739
LouBNielsen <louise.b.nielsen@hotmail.com>
2020-02-03 10:46:02 +0100

    Refactor code as reusable code!

--- js/dillyDally.js
+++ js/dillyDally.js
@@ -1,33 +1,35 @@
 $(function () {
-    $("#timereg_form_part2a").hide();
-    $("#timereg_form_part2b").hide();
+    $("#timereg_form2a").hide();
+    $("#timereg_form2b").hide();
     $("input[type='radio'][name='githubIssue']").change(function () {
         if ($(this).val() == "yes") {
-            $("#timereg_form_part2a").show();
-            $("#timereg_form_part2b").hide();
-            $(':input:not(:button)', $("#timereg_form_part2b")).val('')
+            $("#timereg_form2a").show();
+            $("#timereg_form2b").hide();
+            $(':input:not(:button)', $("#timereg_form2b")).val('')
         }
         else if ($(this).val() == "no") {
-            $("#timereg_form_part2b").show();
-            $("#timereg_form_part2a").hide();
-            $(':input:not(:button)', $("#timereg_form_part2a")).val('')
+            $("#timereg_form2b").show();
+            $("#timereg_form2a").hide();
+            $(':input:not(:button)', $("#timereg_form2a")).val('')
         }
     });
 });
-function validatePart2a() {
-    let form1 = validate("timereg_form_part1");
-    let form2 = validate("timereg_form_part2a")
+
+function validatePostForm(form, form1, form2) {
+    form1 = validate(form1)
+    form2 = validate(form2)
 
     if (form1 && form2) {
-        submitform($("#timereg_form").serializeArray())
+        postForm($('#' + form).serializeArray())
     }
 }
-function validatePart2b() {
-    let form1 = validate("timereg_form_part1");
-    let form2 = validate("timereg_form_part2b")
+
+function validatePutForm(form, form1, form2) {
+    form1 = validate(form1)
+    form2 = validate(form2)
 
     if (form1 && form2) {
-        submitform($("#timereg_form").serializeArray())
+        putForm($('#' + form).serializeArray())
     }
 }
 
@@ -46,7 +48,7 @@ function validate(element) {
     return allAreFilled;
 }
 
-function submitform(data) {
+function postForm(data) {
     let result = {};
     $.each(data, function () {
         var type = document.getElementsByName(this.name)[0].type;
@@ -76,36 +78,80 @@ function submitform(data) {
     })
         .then((res) => {
             if (res.status === 200) {
-                let div = document.createElement('div');
-                div.className = "success";
-                div.innerHTML = "<strong>Worklog added to time registration!</strong>";
-                document.getElementById("success").append(div);
-                setTimeout(function () {
-                    div.style.display = 'none';
-                }, 3000);
+                success();
             } else {
-                let div = document.createElement('div');
-                div.className = "error";
-                div.innerHTML = "<strong>Ups... Worklog not added to time registration.</strong>";
-                document.getElementById("error").append(div);
-                setTimeout(function () {
-                    div.style.display = 'none';
-                }, 3000);
+                error();
             }
         })
         .catch((error) => {
             if (error) {
-                let div = document.createElement('div');
-                div.className = "error";
-                div.innerHTML = "<strong>Ups... Worklog not added to time registration!</strong>";
-                document.getElementById("error").append(div);
-                setTimeout(function () {
-                    div.style.display = 'none';
-                }, 3000);
+                error();
             }
         });
 }
 
+function putForm(data) {
+    let doc_id = document.getElementById('doc_id').value;
+    let result = {};
+    $.each(data, function () {
+        var type = document.getElementsByName(this.name)[0].type;
+        var value = document.getElementsByName(this.name)[0].value;
+
+        if (type === "number") {
+            if (value === "") {
+                result[this.name] = 0;
+            } else {
+                result[this.name] = Number(this.value) ? Number(this.value) : this.value;
+            }
+        } else {
+            result[this.name] = this.value
+        }
+    });
+
+    let workhours = '{"type": "workhours", "fields" : ' + JSON.stringify(result) + ' }';
+
+    axios({
+        method: 'put',
+        url: 'https://us-central1-dillydally-stage.cloudfunctions.net/api/' + doc_id,
+        data: workhours,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    })
+        .then((res) => {
+            if (res.status === 200) {
+                success();
+            } else {
+                error();
+            }
+        })
+        .catch((error) => {
+            if (error) {
+                error();
+            }
+        });
+}
+
+function success() {
+    let div = document.createElement('div');
+    div.className = "success";
+    div.innerHTML = "<strong>Worklog added to time registration!</strong>";
+    document.getElementById("success").append(div);
+    setTimeout(function () {
+        div.style.display = 'none';
+    }, 3000);
+}
+
+function error() {
+    let div = document.createElement('div');
+    div.className = "error";
+    div.innerHTML = "<strong>Ups... Worklog not added to time registration!</strong>";
+    document.getElementById("error").append(div);
+    setTimeout(function () {
+        div.style.display = 'none';
+    }, 3000);
+}
 
 /// Required radio button
 // if (element.type === "radio") {
